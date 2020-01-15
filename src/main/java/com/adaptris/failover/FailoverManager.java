2227cf78abae7ae195dfb68f9d8f0a7cdbd95340
Sebastien Belin <sebastien.belin@adaptris.com>
2020-01-15 08:10:33 +0000

    Fixed compile errors due to removal of commons lang 2.6 dependency.

--- src/main/java/com/adaptris/failover/FailoverManager.java
+++ src/main/java/com/adaptris/failover/FailoverManager.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
