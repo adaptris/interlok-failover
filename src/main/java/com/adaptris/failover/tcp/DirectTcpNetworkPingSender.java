8621ed746d4e2c66766455fd4ac8dc751e00519a
Sebastien Belin <sebastien.belin@adaptris.com>
2022-01-19 03:06:24 +0000

    INTERLOK-3870 Fix potential NPE and resource leak

--- src/main/java/com/adaptris/failover/tcp/DirectTcpNetworkPingSender.java
+++ src/main/java/com/adaptris/failover/tcp/DirectTcpNetworkPingSender.java
@@ -11,34 +11,29 @@ import com.adaptris.failover.Ping;
 import com.adaptris.failover.util.PacketHelper;
 
 public class DirectTcpNetworkPingSender implements NetworkPingSender {
-  
+
   private static final int TIMEOUT_CONNECT = 10000;
 
   private Map<String, InetSocketAddress> cachedAddresses;
-  
+
   public DirectTcpNetworkPingSender() {
     cachedAddresses = new HashMap<>();
   }
 
   @Override
   public void sendData(String host, int port, Ping data) throws Exception {
-    Socket socket = null;
-    
     String mapKey = host + Integer.toString(port);
-    InetSocketAddress inetSocketAddress = this.cachedAddresses.get(mapKey);
+    InetSocketAddress inetSocketAddress = cachedAddresses.get(mapKey);
     if(inetSocketAddress == null) {
       inetSocketAddress = new InetSocketAddress(host, port);
-      this.cachedAddresses.put(mapKey, inetSocketAddress);
+      cachedAddresses.put(mapKey, inetSocketAddress);
     }
-    
-    try {
-      socket = new Socket();
+
+    try (Socket socket = new Socket()) {
       socket.connect(inetSocketAddress, TIMEOUT_CONNECT);
-      
-      DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-      outToServer.write(PacketHelper.createDataPacket(data));
-    } finally {
-      socket.close();
+      try (DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream())) {
+        outToServer.write(PacketHelper.createDataPacket(data));
+      }
     }
   }
 
