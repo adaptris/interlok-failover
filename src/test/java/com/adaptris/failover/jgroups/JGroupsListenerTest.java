52502a95573b08f259a1d08b5eeab1e9d30c9ef3
Sebastien Belin <sebastien.belin@proagrica.com>
2023-06-22 15:10:40 +0900

    Bump org.jgroups:jgroups from 4.2.4.Final to 5.2.14.Final

--- src/test/java/com/adaptris/failover/jgroups/JGroupsListenerTest.java
+++ src/test/java/com/adaptris/failover/jgroups/JGroupsListenerTest.java
@@ -6,8 +6,8 @@ import static org.mockito.Mockito.verify;
 import java.util.Properties;
 import java.util.UUID;
 
+import org.jgroups.BytesMessage;
 import org.jgroups.JChannel;
-import org.jgroups.Message;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -19,74 +19,74 @@ import com.adaptris.failover.util.PacketHelper;
 import junit.framework.TestCase;
 
 public class JGroupsListenerTest extends TestCase {
-  
+
   private static final int SECONDARY = 2;
   private static final int PRIMARY = 1;
-    
+
   private static final String CLUSTER_NAME = "myClusterName";
   private static final String CONFIG_FILE = "./myConfigFile";
-  
+
   private Ping mockPrimaryPing;
   private Ping mockSecondaryPing;
-    
+
   private JGroupsListener jGroupsListener;
   @Mock
   private PingEventListener mockPingEventListener;
   @Mock
   private JChannel mockJChannel;
-  
+
   public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
-    
+
     JGroupsChannel.getInstance().setJGroupsChannel(mockJChannel);
-    
+
     Properties props = new Properties();
     props.put(Constants.FAILOVER_JGROUPS_CONFIG_KEY, CONFIG_FILE);
     props.put(Constants.FAILOVER_JGROUPS_CLUSTER_KEY, CLUSTER_NAME);
-    
+
     jGroupsListener = new JGroupsListener(props);
-        
+
     mockPrimaryPing = new Ping();
     mockPrimaryPing.setInstanceId(UUID.randomUUID());
     mockPrimaryPing.setInstanceType(PRIMARY);
     mockPrimaryPing.setSecondaryNumber(0);
     mockPrimaryPing.setSourceHost("myHost");
     mockPrimaryPing.setSourcePort("1111");
-    
+
     mockSecondaryPing = new Ping();
     mockSecondaryPing.setInstanceId(UUID.randomUUID());
     mockSecondaryPing.setInstanceType(SECONDARY);
     mockSecondaryPing.setSecondaryNumber(1);
     mockSecondaryPing.setSourceHost("myHost");
     mockSecondaryPing.setSourcePort("1111");
-    
+
   }
-  
+
   public void tearDown() throws Exception {
     jGroupsListener.deregisterListener(mockPingEventListener);
     jGroupsListener.stop();
   }
-  
+
   public void testReceivePrimaryPing() throws Exception {
     jGroupsListener.registerListener(mockPingEventListener);
     jGroupsListener.start();
-    
-    jGroupsListener.receive(new Message(null, PacketHelper.createDataPacket(mockPrimaryPing)));
-    
+
+    jGroupsListener.receive(new BytesMessage(null, PacketHelper.createDataPacket(mockPrimaryPing)));
+
     Thread.sleep(3000);
-    
+
     verify(mockPingEventListener).primaryPinged(any(Ping.class));
   }
-  
+
   public void testReceiveSecondaryPing() throws Exception {
     jGroupsListener.registerListener(mockPingEventListener);
     jGroupsListener.start();
-    
-    jGroupsListener.receive(new Message(null, PacketHelper.createDataPacket(mockSecondaryPing)));
-    
+
+    jGroupsListener.receive(new BytesMessage(null, PacketHelper.createDataPacket(mockSecondaryPing)));
+
     Thread.sleep(3000);
-    
+
     verify(mockPingEventListener).secondaryPinged(any(Ping.class));
   }
-  
+
 }
